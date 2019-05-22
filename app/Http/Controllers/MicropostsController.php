bf7d25a8b0b0a5295bca4a4ab81f2c43c1c28427
yusaku.okazaki <include917@gmail.com>
2019-05-22 10:23:29 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -11,7 +11,7 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
             
             $data = [
                 'user' => $user,
@@ -44,6 +44,9 @@ class MicropostsController extends Controller
         }
 
         return back();
-    }    
+    }
+    
+    
+    
     
 }
